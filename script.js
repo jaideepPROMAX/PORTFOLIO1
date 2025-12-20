d53be082a75e41241a21dfc334f463b2b5b0f341
jaideepPROMAX <jaideepeemani@gmail.com>
2025-12-20 20:32:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function openProjects() {
-    window.open("projects.html", "_blank");
-}
+const projectsBtn = document.getElementById("projectsBtn");
 
+if (projectsBtn) {
+    projectsBtn.addEventListener("click", function () {
+        window.location.href = "projects.html";
+    });
+}
